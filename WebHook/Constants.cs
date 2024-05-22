491d13a8dbc4a501d46026386ee4847156990c42
shahzod.akhmedov <shahzod.akhmedov@bk.ru>
2024-05-22 12:28:38 +0300

    hotpush

--- WebHook/Constants.cs
+++ WebHook/Constants.cs
@@ -1,6 +1,12 @@
 ﻿namespace WebHook
 {
-    public class Constants
+    public static class Auth_token
     {
+        public const string qolio_auth_token = "7f5c3801879284bdacb704a8cc8a25ff21f2af49a819cafe931b6f5750f6fbbfbc9686b05ba7f525d34ef075261c56e0f2bb1c7bda5b43e80922c55c5189e195";
+    }
+    public static class Qolio_Credetials
+    {
+        public const string Login = "shahzod.akhmedov@bk.ru";
+        public const string Password = "@5I4Qk~qJTTR2s#";
     }
 }
